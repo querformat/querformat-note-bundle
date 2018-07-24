353b0ded83ec4a159b00ea2b5ca2df8d09d5b2ba
MariaEngel <engel@querformat.info>
2018-07-24 15:06:58 +0200

    config backend menu

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['note'][0] = 'Notizen';
+$GLOBALS['TL_LANG']['MOD']['note'][1] = 'Notizen verwalten';
--- src/Resources/contao/languages/modules.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['MOD']['note'][0] = 'Notiz';
-$GLOBALS['TL_LANG']['MOD']['note'][1] = 'Notiz verwalten';
