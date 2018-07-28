2a75e7da055b98357b06baf6eb447c45a002a50e
MariaEngel <engel@querformat.info>
2018-07-28 17:26:44 +0200

    [feature] optimized extension main class NotesPlugin

--- src/NotesPlugin.php
+++ src/NotesPlugin.php
@@ -14,25 +14,42 @@ class NotesPlugin
      * @param $strBuffer
      * @return string
      */
-    public function myGetContentElement(ContentModel $objElement, $strBuffer)
+    public function displayNotes(ContentModel $objElement, $strBuffer)
     {
 
-        $currentUser = BackendUser::getInstance();
-        $currentUserShowNotes = $currentUser->getData()['qfNoteFeActive'];
+        if ($this->showNotesToCurrentUser()):
 
-        if ($currentUserShowNotes):
+            $note = $this->getNoteIfExists($objElement->id);
 
-            $db = Database::getInstance();
-            $currentElement = $db->execute('SELECT qfNoteActivate,qfNoteText,qfNoteTitle FROM tl_content WHERE id=' . $objElement->id)->fetchAssoc();
-
-            if ($currentElement['qfNoteActivate'] == 1) {
-                return $strBuffer . '<h3>' . $currentElement['qfNoteTitle'] . '</h3>' . '<p>' . $currentElement['qfNoteText'] . '</p>';
+            if ($note != false && $note['qfNoteActivate'] == 1) {
+                return $strBuffer . '<h3>' . $note['qfNoteTitle'] . '</h3>' . '<p>' . $note['qfNoteText'] . '</p>';
             }
 
         endif;
 
         return $strBuffer;
 
+    }
+
+    /**
+     * @param $contentId
+     * @return array|false
+     */
+    private function getNoteIfExists($contentId)
+    {
+        $db = Database::getInstance();
+        return $db->prepare('SELECT qfNoteActivate,qfNoteText,qfNoteTitle FROM tl_content WHERE id=?')
+            ->execute($contentId)
+            ->fetchAssoc();
+    }
+
+    /**
+     * @return boolean
+     */
+    private function showNotesToCurrentUser()
+    {
+        $currentUser = BackendUser::getInstance();
+        return $currentUser->getData()['qfNoteFeActive'];
     }
 
 
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,15 +8,18 @@
  * @license LGPL-3.0-or-later
  */
 
-// Back end modules
-
+// Init backend menu item
+/*
 $GLOBALS['BE_MOD']['content']['note'] = array(
     'tables' => array('tl_content')
 );
+*/
 
+// Integrate extension specific javascript and css files
 if (TL_MODE == 'FE') {
     $GLOBALS['TL_CSS'][] = 'bundles/querformatnote/assets/css/qfNotesPlugin.css|static';
     $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/querformatnote/assets/js/qfNotesPlugin.js|static';
 }
 
-$GLOBALS['TL_HOOKS']['getContentElement'][] = ['Querformat\\NoteBundle\\NotesPlugin', 'myGetContentElement'];
+// Hooks
+$GLOBALS['TL_HOOKS']['getContentElement'][] = ['Querformat\\NoteBundle\\NotesPlugin', 'displayNotes'];
